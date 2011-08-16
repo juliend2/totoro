2d8b47fdf4af2ca36e2aabe49a5ae968c8318c6b
Julien Desrosiers <juliend2@gmail.com>
2011-08-15 20:11:05 -0400

    added date

--- views/home.php
+++ views/home.php
@@ -1,4 +1,5 @@
 <?php foreach($posts as $post):?>
   <h2><a href="<?php echo post_url($post) ?>"><?php echo $post['title'] ?></a></h2>
+  <h3><?php echo strftime('%B %e, %Y', strtotime($post['year'].'-'.$post['month'].'-'.$post['day'])) ?></h3>
   <?php echo $post['content'] ?>
 <?php endforeach ?>
