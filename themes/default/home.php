e83da183aa41516b9d7a690b3de4db9e72ddf0e3
Julien Desrosiers <juliend2@gmail.com>
2011-12-31 21:33:39 -0500

    default theme style changes

--- themes/default/home.php
+++ themes/default/home.php
@@ -1,5 +1,5 @@
 <?php foreach($posts as $post):?>
   <h2><a href="<?php echo post_url($post) ?>"><?php echo $post['title'] ?></a></h2>
-  <h3><?php echo strftime('%B %e, %Y', strtotime($post['year'].'-'.$post['month'].'-'.$post['day'])) ?></h3>
+  <h3 class="date"><?php echo strftime('%B %e, %Y', strtotime($post['year'].'-'.$post['month'].'-'.$post['day'])) ?></h3>
   <?php echo excerpt($post['content']) ?> 
 <?php endforeach ?>
