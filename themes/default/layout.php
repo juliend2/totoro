ca2d963db05c41f39b9e0b5ed862db8bfe662e36
Julien Desrosiers <juliend2@gmail.com>
2011-12-31 21:55:54 -0500

    styles for footer. added rss link + meta description in head

--- themes/default/layout.php
+++ themes/default/layout.php
@@ -3,8 +3,10 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title><?php echo $config['blog_title'] ?></title>
+  <meta name="description" content="<?php echo $config['blog_description'] ?>" />
   <link rel="stylesheet" type="text/css" href="<?php echo theme_url() ?>/css/reset.css"/>
   <link rel="stylesheet/less" type="text/css" href="<?php echo theme_url() ?>/css/styles.less"/>
+  <link rel="alternate" type="application/rss+xml" title="<?php echo $config['blog_title'] ?>" href="<?php echo $config['base_url'] ?>/rss" />
   <script src="<?php echo theme_url() ?>/js/less-1.1.5.min.js" type="text/javascript"></script>
 </head>
 <body>
@@ -19,7 +21,7 @@
 </section>
 
 <footer>
-<?php echo $config['author_name'] ?>. [<a href="<?php echo $config['base_url'] ?>/rss">rss</a>]
+  &copy; <?php echo date('Y') ?> <?php echo $config['author_name'] ?>. <br/>Powered by <a href="https://github.com/juliend2/bato">Bato</a>.
 </footer>
 
 </body>
